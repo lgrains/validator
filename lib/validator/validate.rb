32334df8a50e9f171ea1421df5f1a80e4758067a
Louise Rains <lrains@berklee.edu>
2013-12-18 18:12:06 -0500

    removes print statements

--- lib/validator/validate.rb
+++ lib/validator/validate.rb
@@ -4,7 +4,6 @@ require "sudoku_grid"
 module Validator
   class Validate
     def self.validate_grid(filename)
-      puts "in Validator::Validate where filename is #{filename}"
       grid = SudokuGrid.new(filename)
       grid.sudoku_state
     end
